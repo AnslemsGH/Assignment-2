9a2729385a0b96579004be1a707f5128c4c89ef7
Anslem Coelho <anslem1999@gmail.com>
2025-06-19 19:54:04 -0400

    J2

--- Assignment 2/Controllers/J2Controller.cs
+++ Assignment 2/Controllers/J2Controller.cs
@@ -7,5 +7,43 @@ namespace Assignment_2.Controllers
     [ApiController]
     public class J2Controller : ControllerBase
     {
+        [HttpGet(template: "ChiliPeppers")]
+
+        public int ChiliPeppers(string Ingredients)
+        {
+            int spice = 0;
+
+            string[] ron = Ingredients.Split(",");
+
+            for (int i = 0; i < ron.Length; i++)
+            {
+
+                if (ron[i] == "Habanero")
+                {
+                    spice = spice + 125000;
+                }
+                else if (ron[i] == "Poblano")
+                {
+                    spice = spice + 1500;
+                }
+                else if (ron[i] == "Mirasol")
+                {
+                    spice = spice + 6000;
+                }
+                else if (ron[i] == "Serrano")
+                {
+                    spice = spice + 15500;
+                }
+                else if (ron[i] == "Cayenne")
+                {
+                    spice = spice + 40000;
+                }
+                else if (ron[i] == "Thai")
+                {
+                    spice = spice + 75000;
+                }
+            }
+            return spice;
+        }
     }
 }
