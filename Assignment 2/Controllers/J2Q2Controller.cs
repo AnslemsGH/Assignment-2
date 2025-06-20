46fa7a0428ee3a22cf03a92be8299f4ccb50dd6e
Anslem Coelho <anslem1999@gmail.com>
2025-06-20 10:03:50 -0400

    J1Q2

---
+++ Assignment 2/Controllers/J2Q2Controller.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment_2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class J2Q2Controller : ControllerBase
+    {
+        [HttpPost (template:"Mood")]
+
+        public string Mood([FromForm] string Message)
+        {
+            int happy = 0;
+            int sad = 0;
+
+            int i = 0;
+            while (i <= (Message.Length-3))
+            {
+                string characters = Message.Substring(i, 3);
+                if (characters == ":-)")
+                {
+                    happy = happy + 1;
+                }
+                else if (characters == ":-(")
+                {
+                    sad = sad + 1;
+                }
+                i = i + 1;
+            }
+
+            string mood = "";
+
+            if (happy == 0 && sad == 0)
+                mood = "None";
+            else if (happy == sad)
+                mood = "Unsure";
+            else if (happy > sad)
+                mood = "Happy";
+            else
+                mood = "Sad";
+
+            return mood;
+        }
+    }
+}
